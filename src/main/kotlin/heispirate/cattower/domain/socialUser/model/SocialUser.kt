ada88dd7c6a85a6c41af88f9a5e4e1c5e97963a5
dyorcat <yool0417@gmail.com>
2024-05-20 12:52:57 +0900

    수정 : SocialUser Entity에서 Id 삭제

--- src/main/kotlin/heispirate/cattower/domain/socialUser/model/SocialUser.kt
+++ src/main/kotlin/heispirate/cattower/domain/socialUser/model/SocialUser.kt
@@ -1,13 +1,10 @@
 package heispirate.cattower.domain.socialUser.model
 
-import heispirate.cattower.infra.BaseTimeEntity
+import heispirate.cattower.infra.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Table(name = "socialUser")
@@ -26,8 +23,6 @@ class SocialUser(
     @Column(name = "email")
     val email : String,
 
-): BaseTimeEntity() {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
+): BaseEntity() {
+
 }
