3a5b32e0a8915d15a88938de6cc8275ed9cabadf
dyorcat <yool0417@gmail.com>
2024-05-20 12:51:59 +0900

    수정 : Comment Entity에서 Id 삭제

--- src/main/kotlin/heispirate/cattower/domain/comment/model/Comment.kt
+++ src/main/kotlin/heispirate/cattower/domain/comment/model/Comment.kt
@@ -2,13 +2,10 @@ package heispirate.cattower.domain.comment.model
 
 import heispirate.cattower.domain.petProfile.model.PetProfile
 import heispirate.cattower.domain.post.model.Post
-import heispirate.cattower.infra.BaseTimeEntity
+import heispirate.cattower.infra.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
@@ -33,8 +30,6 @@ class Comment(
     @JoinColumn(name = "petProfileId")
     val petProfile: PetProfile,
 
-) : BaseTimeEntity() {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
+) : BaseEntity() {
+
 }
