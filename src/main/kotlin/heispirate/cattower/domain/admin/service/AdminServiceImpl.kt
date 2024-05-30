290a617e71dba804743de110ed55fd0193a0ae00
Karox1234 <zzzcomet@gmail.com>
2024-05-30 11:21:27 +0900

    수정 : 카테고리를 직접 참조, 그로 인한 로직 변경 추가 : 테스트시 어느 시점에서 삭제되는지 체크를 위한 print추가

--- src/main/kotlin/heispirate/cattower/domain/admin/service/AdminServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/admin/service/AdminServiceImpl.kt
@@ -5,19 +5,18 @@ import heispirate.cattower.domain.admin.repository.AdminRepository
 import heispirate.cattower.domain.petProfile.model.PetProfile
 import heispirate.cattower.domain.post.model.Post
 import heispirate.cattower.infra.category.Category
-import heispirate.cattower.infra.category.CategoryInterface
 import org.springframework.stereotype.Service
 
 @Service
 class AdminServiceImpl(private val adminRepository: AdminRepository) : AdminService {
-    override fun checkAdmin(email: String, categorySelect: CategoryInterface): Boolean {
+    override fun checkAdmin(email: String, categorySelect: Category): Boolean {
         val admin = adminRepository.findByMainUserEmail(email) ?: throw IllegalArgumentException("이메일을 찾을수 없습니다")
         return when(admin.role) {
             Role.ADMIN -> true
-            Role.DAILY_LIFE_MANAGER -> categorySelect.category == Category.DAILY_LIFE
-            Role.INFORMATION_MANAGER -> categorySelect.category == Category.INFORMATION
-            Role.QUESTION_MANAGER -> categorySelect.category == Category.QUESTION
-            Role.SHARING_MANAGER -> categorySelect.category == Category.SHARING
+            Role.DAILY_LIFE_MANAGER -> categorySelect == Category.DAILY_LIFE
+            Role.INFORMATION_MANAGER -> categorySelect == Category.INFORMATION
+            Role.QUESTION_MANAGER -> categorySelect == Category.QUESTION
+            Role.SHARING_MANAGER -> categorySelect == Category.SHARING
         }
     }
 
@@ -27,11 +26,13 @@ class AdminServiceImpl(private val adminRepository: AdminRepository) : AdminServ
     }
 
     // 적용 예시
-    private fun checkAdminExample(email: String, post: Post, petProfile: PetProfile) {
-        if (checkAdmin(email,post)) {
+    fun checkAdminTest(email: String, post: Post, petProfile: PetProfile) {
+        if (checkAdmin(email, post.category)) {
             // Post 삭제 로직
+            println("Post 삭제됨: ${post.id} by Admin")
         } else if (userCheck(post, petProfile)) {
             // Post 삭제 로직
+            println("Post 삭제됨: ${post.id} by User")
         } else {
             throw IllegalStateException("삭제할 권한이 없습니다.")
         }
