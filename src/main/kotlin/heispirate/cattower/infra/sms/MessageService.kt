bd67a672179adbf7cc34c73b8bb1f613af509dec
Karox1234 <zzzcomet@gmail.com>
2024-06-04 20:37:43 +0900

    수정 : from 내 번호 사용(임시) 추가 : repository에 메시지 내용 저장 추가

--- src/main/kotlin/heispirate/cattower/infra/sms/MessageService.kt
+++
@@ -1,7 +0,0 @@
-package heispirate.cattower.infra.sms
-
-import net.nurigo.sdk.message.response.SingleMessageSentResponse
-
-interface MessageService {
-    fun sendOne(from: String, to: String, text: String): SingleMessageSentResponse?
-}
