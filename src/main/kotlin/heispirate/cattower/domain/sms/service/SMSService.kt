90fd6ccc3410f786b42bb626d0e5410bd606b6eb
Karox1234 <zzzcomet@gmail.com>
2024-06-06 21:51:36 +0900

    추가 : 인증번호 발송을 위한 로직 추가

--- src/main/kotlin/heispirate/cattower/domain/sms/service/SMSService.kt
+++ src/main/kotlin/heispirate/cattower/domain/sms/service/SMSService.kt
@@ -4,4 +4,6 @@ import net.nurigo.sdk.message.response.SingleMessageSentResponse
 
 interface SMSService {
     fun sendOne(to: String, text: String): SingleMessageSentResponse?
+
+    fun sendVerificationCode(to: String): SingleMessageSentResponse?
 }
