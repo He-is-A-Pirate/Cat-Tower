0bcc12bbcd2f421525b77ba9140008a59944356c
dyorcat <yool0417@gmail.com>
2024-06-07 12:49:49 +0900

    추가 : PetProfileServiceImpl 작성. createPetProfile 메서드 구현

--- src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
@@ -1,7 +1,32 @@
 package heispirate.cattower.domain.petProfile.service
 
+import heispirate.cattower.domain.mainUser.repository.MainUserRepository
+import heispirate.cattower.domain.petProfile.dto.CreatePetProfileRequestDTO
+import heispirate.cattower.domain.petProfile.dto.PetProfileResponseDTO
+import heispirate.cattower.domain.petProfile.repository.PetProfileRepository
+import heispirate.cattower.exception.ModelNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PetProfileServiceImpl : PetProfileService {
+class PetProfileServiceImpl(
+    private val mainUserRepository: MainUserRepository,
+    private val petProfileRepository: PetProfileRepository
+): PetProfileService {
+    @Transactional
+    override fun createPetProfile(
+        userId: Long,
+        createPetProfileRequestDTO: CreatePetProfileRequestDTO
+    ): PetProfileResponseDTO {
+        val mainUser = mainUserRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("user", userId)
+        val petProfiles = petProfileRepository.findAllByMainUser(mainUser)
+        if (petProfiles.isNotEmpty() && petProfiles.size >= 3) {
+            throw Exception("펫 프로필 생성은 3개를 초과할 수 없습니다.")
+        }
+        val newPetProfile = createPetProfileRequestDTO.toEntity().apply { this.mainUser = mainUser }
+        mainUser.petProfiles?.add(newPetProfile)
+        val savedPetProfile = petProfileRepository.save(newPetProfile)
+        return PetProfileResponseDTO.toResponse(savedPetProfile)
+    }
 }
