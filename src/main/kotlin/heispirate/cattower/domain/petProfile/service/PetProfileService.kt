6ba036d2718f378da02a7bddbf15e93d8965a27f
dyorcat <yool0417@gmail.com>
2024-06-07 12:49:22 +0900

    추가 : PetProfileService 작성. createPetProfile 메서드 추가

--- src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileService.kt
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileService.kt
@@ -1,4 +1,9 @@
 package heispirate.cattower.domain.petProfile.service
 
+import heispirate.cattower.domain.petProfile.dto.CreatePetProfileRequestDTO
+import heispirate.cattower.domain.petProfile.dto.PetProfileResponseDTO
+
 interface PetProfileService {
+    fun createPetProfile(userId: Long, createPetProfileRequestDTO: CreatePetProfileRequestDTO
+    ): PetProfileResponseDTO
 }
