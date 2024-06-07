8c1130cfdba3f241c16acbb9e08afc375366b1a4
dyorcat <yool0417@gmail.com>
2024-06-07 10:57:46 +0900

    추가 : CreatePetProfileRequestDTO 추가

---
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/dto/CreatePetProfileRequestDTO.kt
@@ -0,0 +1,37 @@
+package heispirate.cattower.domain.petProfile.dto
+
+import heispirate.cattower.domain.petProfile.model.Gender
+import heispirate.cattower.domain.petProfile.model.PetProfile
+import java.time.LocalDateTime
+
+data class CreatePetProfileRequestDTO(
+    val name: String,
+    val gender: Gender,
+    val birthDay: LocalDateTime,
+    val age: Int,
+    val kind: String?,
+    val address: String?,
+    val aboutMe: String?,
+    val bloodType: String?,
+    val weight: Double?,
+    val healthHistory: String?,
+    val profileImageUrl: String?,
+    val disclosure: Boolean,
+) {
+    fun toEntity(): PetProfile {
+            return PetProfile(
+                name = name,
+                gender = gender,
+                birthDay = birthDay,
+                age = age,
+                kind = kind,
+                address = address,
+                aboutMe = aboutMe,
+                bloodType = bloodType,
+                weight = weight,
+                healthHistory = healthHistory,
+                profileImageUrl = profileImageUrl,
+                disclosure = disclosure,
+            )
+    }
+}
