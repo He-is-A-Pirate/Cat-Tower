6fc3bc480dcd452671f419026ddc00eedc9cabee
dyorcat <yool0417@gmail.com>
2024-06-07 12:47:35 +0900

    추가 : PetProfileController 작성. createPetProfile 메서드 추가

--- src/main/kotlin/heispirate/cattower/domain/petProfile/controller/PetProfileController.kt
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/controller/PetProfileController.kt
@@ -1,7 +1,30 @@
 package heispirate.cattower.domain.petProfile.controller
 
+import heispirate.cattower.domain.petProfile.dto.CreatePetProfileRequestDTO
+import heispirate.cattower.domain.petProfile.dto.PetProfileResponseDTO
+import heispirate.cattower.domain.petProfile.service.PetProfileService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PetProfileController {
+@RequestMapping("/users/{userId}/pets")
+class PetProfileController(
+    private val petProfileService: PetProfileService
+) {
+    @PostMapping()
+    fun createPetProfile(
+        @PathVariable userId: Long,
+        @RequestBody createPetProfileRequestDTO: CreatePetProfileRequestDTO
+    ): ResponseEntity<PetProfileResponseDTO> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(petProfileService.createPetProfile(userId, createPetProfileRequestDTO))
+    }
+
+
 }
