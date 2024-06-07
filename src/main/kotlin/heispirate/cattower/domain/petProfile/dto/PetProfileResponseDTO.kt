8ff9081fe23c78f671abfeb42de30badf695c159
dyorcat <yool0417@gmail.com>
2024-06-07 12:48:57 +0900

    추가 : PetProfileResponseDTO 작성

---
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/dto/PetProfileResponseDTO.kt
@@ -0,0 +1,39 @@
+package heispirate.cattower.domain.petProfile.dto
+
+import heispirate.cattower.domain.petProfile.model.Gender
+import heispirate.cattower.domain.petProfile.model.PetProfile
+import java.time.LocalDateTime
+data class PetProfileResponseDTO(
+    val name: String,
+    val gender: Gender,
+    val birthday: LocalDateTime,
+    val age: Int,
+    val kind: String?,
+    val address: String?,
+    val aboutMe: String?,
+    val bloodType: String?,
+    val weight: Double?,
+    val healthHistory: String?,
+    val profileImageUrl: String?,
+    val disclosure: Boolean
+) {
+
+    companion object {
+        fun toResponse(petProfile: PetProfile): PetProfileResponseDTO {
+            return PetProfileResponseDTO(
+                name = petProfile.name,
+                gender = petProfile.gender,
+                birthday = petProfile.birthDay,
+                age = petProfile.age,
+                kind = petProfile.kind,
+                address = petProfile.address,
+                aboutMe = petProfile.aboutMe,
+                bloodType = petProfile.bloodType,
+                weight = petProfile.weight,
+                healthHistory = petProfile.healthHistory,
+                profileImageUrl = petProfile.profileImageUrl,
+                disclosure = petProfile.disclosure,
+            )
+        }
+    }
+}
