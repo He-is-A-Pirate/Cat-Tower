367a743cde86bf536f0f07a3df96b5a5b138c79e
dyorcat <yool0417@gmail.com>
2024-06-09 14:51:54 +0900

    수정 : getUersPetProfileList 메서드 구현

--- src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
@@ -1,7 +1,7 @@
 package heispirate.cattower.domain.petProfile.service
 
 import heispirate.cattower.domain.mainUser.repository.MainUserRepository
-import heispirate.cattower.domain.petProfile.dto.CreatePetProfileRequestDTO
+import heispirate.cattower.domain.petProfile.dto.PetProfileRequestDTO
 import heispirate.cattower.domain.petProfile.dto.PetProfileResponseDTO
 import heispirate.cattower.domain.petProfile.repository.PetProfileRepository
 import heispirate.cattower.exception.ModelNotFoundException
@@ -17,20 +17,30 @@ class PetProfileServiceImpl(
     @Transactional
     override fun createPetProfile(
         userId: Long,
-        createPetProfileRequestDTO: CreatePetProfileRequestDTO
+        petProfileRequestDTO: PetProfileRequestDTO
     ): PetProfileResponseDTO {
         val mainUser = mainUserRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("user", userId)
         val petProfiles = petProfileRepository.findAllByMainUser(mainUser)
         if (petProfiles.isNotEmpty() && petProfiles.size >= 3) {
             throw Exception("펫 프로필 생성은 3개를 초과할 수 없습니다.")
         }
-        val newPetProfile = createPetProfileRequestDTO.toEntity().apply { this.mainUser = mainUser }
-        mainUser.petProfiles?.add(newPetProfile)
+        val newPetProfile = petProfileRequestDTO.toEntity().apply { this.mainUser = mainUser }
         val savedPetProfile = petProfileRepository.save(newPetProfile)
+        mainUser.petProfiles?.add(newPetProfile)
         return PetProfileResponseDTO.toResponse(savedPetProfile)
     }
-    override fun getPetProfile(petId: Long): PetProfileResponseDTO {
+    override fun getPetProfile(
+        userId: Long,
+        petId: Long
+    ): PetProfileResponseDTO {
+        mainUserRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("user", userId)
         val petProfile = petProfileRepository.findByIdOrNull(petId) ?: throw ModelNotFoundException("pet", petId)
         return PetProfileResponseDTO.toResponse(petProfile)
+    }
+
+    override fun getUsersPetProfileList(userId: Long): List<PetProfileResponseDTO> {
+        val mainUser = mainUserRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("user", userId)
+        val petProfiles = petProfileRepository.findAllByMainUser(mainUser)
+        return petProfiles.map { PetProfileResponseDTO.toResponse(it) }
     }
 }
