9808b030c5673cdd29db4741b85f80bd18de4f7f
dyorcat <yool0417@gmail.com>
2024-06-14 17:49:23 +0900

    수정 : petProfileRequestDTO -> request 로 변경

--- src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/petProfile/service/PetProfileServiceImpl.kt
@@ -17,39 +17,40 @@ class PetProfileServiceImpl(
     @Transactional
     override fun createPetProfile(
         userId: Long,
-        petProfileRequestDTO: PetProfileRequestDTO
+        request: PetProfileRequestDTO
     ): PetProfileResponseDTO {
         val mainUser = mainUserRepository.findByIdAndDeletedAtIsNull(userId) ?: throw ModelNotFoundException("user", userId)
         val petProfiles = petProfileRepository.findAllByMainUserAndDeletedAtIsNull(mainUser)
         if (petProfiles.size >= 3) {
             throw Exception("펫 프로필 생성은 3개를 초과할 수 없습니다.")
         }
-        val newPetProfile = petProfileRequestDTO.toEntity(mainUser) // .apply { this.mainUser = mainUser }
+        val newPetProfile = request.toEntity(mainUser)
         val savedPetProfile = petProfileRepository.save(newPetProfile)
-        // mainUser.petProfiles?.add(newPetProfile)
         return PetProfileResponseDTO.toResponse(savedPetProfile)
     }
+
+    @Transactional
     override fun getPetProfile(
         userId: Long,
         petId: Long
     ): PetProfileResponseDTO {
+        mainUserRepository.findByIdAndDeletedAtIsNull(userId) ?: throw ModelNotFoundException("user", userId)
         val petProfile =
             petProfileRepository.findByIdAndDeletedAtIsNull(petId) ?: throw ModelNotFoundException("pet", petId)
-        val mainUser = mainUserRepository.findByIdAndDeletedAtIsNull(userId) ?: throw ModelNotFoundException("user", userId)
-        if (!petProfile.disclosure && petProfile.mainUser?.id != mainUser.id) {
-                throw Exception("비공개 프로필입니다.")
-            }
         return PetProfileResponseDTO.toResponse(petProfile)
     }
 
-    override fun getUsersPetProfileList(userId: Long): List<PetProfileResponseDTO> {
+    @Transactional
+    override fun getUsersPetProfileList(
+        userId: Long
+    ): List<PetProfileResponseDTO> {
         val mainUser = mainUserRepository.findByIdAndDeletedAtIsNull(userId) ?: throw ModelNotFoundException("user", userId)
-        val petProfiles = petProfileRepository.findAllByMainUserAndDeletedAtIsNull(mainUser).filter{it.disclosure}
+        val petProfiles = petProfileRepository.findAllByMainUserAndDeletedAtIsNull(mainUser)
         return petProfiles.map { PetProfileResponseDTO.toResponse(it) }
     }
 
     @Transactional
-    override fun updatePetProfile(userId: Long, petId: Long, petProfileRequestDTO: PetProfileRequestDTO
+    override fun updatePetProfile(userId: Long, petId: Long, request: PetProfileRequestDTO
     ): PetProfileResponseDTO {
         val mainUser = mainUserRepository.findByIdAndDeletedAtIsNull(userId) ?: throw ModelNotFoundException("user", userId)
         val petProfile = petProfileRepository.findByIdAndDeletedAtIsNull(petId) ?: throw ModelNotFoundException("pet", petId)
@@ -57,18 +58,17 @@ class PetProfileServiceImpl(
         if (petProfile.mainUser != mainUser) {
             throw Exception("해당 유저의 펫프로필이 아닙니다.")
         }
-        petProfile.apply { name = petProfileRequestDTO.name
-            gender = petProfileRequestDTO.gender
-            birthDay = petProfileRequestDTO.birthDay
-            age = petProfileRequestDTO.age
-            kind = petProfileRequestDTO.kind
-            address = petProfileRequestDTO.address
-            aboutMe = petProfileRequestDTO.aboutMe
-            bloodType = petProfileRequestDTO.bloodType
-            weight = petProfileRequestDTO.weight
-            healthHistory = petProfileRequestDTO.healthHistory
-            profileImageUrl = petProfileRequestDTO.profileImageUrl
-            disclosure = petProfileRequestDTO.disclosure}
+        petProfile.apply { name = request.name
+            gender = request.gender
+            birthDay = request.birthDay
+            age = request.age
+            kind = request.kind
+            address = request.address
+            aboutMe = request.aboutMe
+            bloodType = request.bloodType
+            weight = request.weight
+            healthHistory = request.healthHistory
+            profileImageUrl = request.profileImageUrl }
 
         return PetProfileResponseDTO.toResponse(petProfile)
     }
