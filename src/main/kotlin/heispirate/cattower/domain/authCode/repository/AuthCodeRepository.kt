4c636093d2ac5786da789282a7e5c16cc3d30707
kimboseong <njunju77@nate.com>
2024-06-18 08:33:01 +0900

    추가: 이메일로 유효한 1개의 코드를 찾는 findByEmailAndAvailableIsTrue 함수 추가

--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/AuthCodeRepository.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/AuthCodeRepository.kt
@@ -4,4 +4,6 @@ import heispirate.cattower.domain.authCode.model.AuthCode
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AuthCodeRepository : JpaRepository<AuthCode,Long> , CustomAuthCodeRepository {
+
+    fun findByEmailAndAvailableIsTrue(email: String): AuthCode?
 }
