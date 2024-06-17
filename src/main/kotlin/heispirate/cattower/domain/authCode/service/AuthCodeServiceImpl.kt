c671684016e31f7bc4feb8ec359f0745fb909fb7
kimboseong <njunju77@nate.com>
2024-06-18 08:35:11 +0900

    추가: 메일전송/ 인증코드 검증 서비스 구현체 추가

--- src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeServiceImpl.kt
@@ -1,7 +1,51 @@
 package heispirate.cattower.domain.authCode.service
 
+import heispirate.cattower.domain.authCode.dto.AuthResponseDTO
+import heispirate.cattower.domain.authCode.repository.AuthCodeRepository
+import heispirate.cattower.infra.email.EmailUtility
+import jakarta.transaction.Transactional
+import java.time.LocalDateTime
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
-class AuthCodeServiceImpl : AuthCodeService{
+class AuthCodeServiceImpl(
+    private val authCodeRepository: AuthCodeRepository,
+    private val emailUtility: EmailUtility,
+    private val javaMailSender: JavaMailSender,
+) : AuthCodeService {
+    @Transactional
+    override fun sendAuthEmail(email: String): AuthResponseDTO {
+        emailUtility.requestAuthCode(email)
+        val message = "이메일 전송완료"
+        return AuthResponseDTO(
+            message = message
+        )
+    }
+    @Transactional
+    override fun verifyCode(email: String,code: String): AuthResponseDTO {
+        val authCode = authCodeRepository.findByEmailAndAvailableIsTrue(email)?: throw Exception("유효한 인증코드가 없습니다")
+        val now = LocalDateTime.now()
+        val verifyMessage = "인증이 완료되었습니다"
+
+        return if (authCode.code == code && authCode.expirationTime > now){
+            authCode.available = false
+            AuthResponseDTO(
+                message = verifyMessage
+            )
+        }else{
+            throw Exception("인증코드 불일치")
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
