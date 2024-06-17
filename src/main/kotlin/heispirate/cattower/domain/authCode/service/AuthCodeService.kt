968ea9c6e3b656faf25dc31f1d99924164999482
kimboseong <njunju77@nate.com>
2024-06-18 08:30:27 +0900

    추가: 이메일 전송 / 인증코드 검증 서비스 인터페이스 추가

--- src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeService.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeService.kt
@@ -1,4 +1,9 @@
 package heispirate.cattower.domain.authCode.service
 
+import heispirate.cattower.domain.authCode.dto.AuthResponseDTO
+
 interface AuthCodeService {
+    fun sendAuthEmail(email: String):AuthResponseDTO
+
+    fun verifyCode(email: String, code: String): AuthResponseDTO
 }
