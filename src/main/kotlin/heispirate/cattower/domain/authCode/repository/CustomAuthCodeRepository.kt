f1ae874791d7cdc8d5698c673b4a07fe36165ee4
kimboseong <njunju77@nate.com>
2024-06-18 10:58:25 +0900

    추가: 00시 기준 유효시간이 만료한 코드를 지우는 커스텀 쿼리 메서드 추가

--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomAuthCodeRepository.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomAuthCodeRepository.kt
@@ -1,4 +1,7 @@
 package heispirate.cattower.domain.authCode.repository
 
+import java.time.LocalDateTime
+
 interface CustomAuthCodeRepository {
+    fun deleteByExpirationTime(zeroHour: LocalDateTime)
 }
--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomCustomAuthCodeRepositoryImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomCustomAuthCodeRepositoryImpl.kt
@@ -1,6 +1,20 @@
 package heispirate.cattower.domain.authCode.repository
 
+import heispirate.cattower.domain.authCode.model.QAuthCode
 import heispirate.cattower.infra.QueryDslSupport
+import java.time.LocalDateTime
+import org.springframework.stereotype.Repository
 
+@Repository
 class CustomCustomAuthCodeRepositoryImpl : CustomAuthCodeRepository , QueryDslSupport() {
+    private val authCode = QAuthCode.authCode
+    override fun deleteByExpirationTime(zeroHour: LocalDateTime) {
+
+        queryFactory.delete(authCode)
+            .where(authCode.expirationTime.lt(zeroHour))
+            .execute()
+
+    }
+
+
 }
