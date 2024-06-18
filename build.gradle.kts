b6c5030c45cbf20a36cede102ffdc399e011b209
kimboseong <njunju77@nate.com>
2024-06-18 11:59:58 +0900

    추가: logging 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,10 @@ dependencies {
 
     //mail
     implementation ("org.springframework.boot:spring-boot-starter-mail")
+
+    //log
+    implementation("org.slf4j:slf4j-api:1.7.32")  // SLF4J API
+    implementation("ch.qos.logback:logback-classic:1.4.12")  // Logback
 }
 
 tasks.withType<KotlinCompile> {
