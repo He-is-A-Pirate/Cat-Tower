6db1071312a437287099c24c130aa07fe283c61b
kimboseong <njunju77@nate.com>
2024-06-19 12:37:21 +0900

    추가: JPQL 메서드 추가

--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/AuthCodeRepository.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/AuthCodeRepository.kt
@@ -1,11 +1,18 @@
 package heispirate.cattower.domain.authCode.repository
 
 import heispirate.cattower.domain.authCode.model.AuthCode
+import java.time.LocalDateTime
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface AuthCodeRepository : JpaRepository<AuthCode,Long> , CustomAuthCodeRepository {
 
     fun findByEmailAndAvailableIsTrue(email: String): AuthCode?
+
+    @Modifying
+    @Query("DELETE FROM AuthCode a WHERE a.expirationTime < :zeroHour")
+    fun deleteByExpirationTime(zeroHour:LocalDateTime): Int
 
 
 }
