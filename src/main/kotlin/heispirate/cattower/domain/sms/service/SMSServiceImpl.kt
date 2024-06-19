0492a0e3f6974a5df04824076dcbd20ac863802b
kimboseong <njunju77@nate.com>
2024-06-19 13:21:39 +0900

    수정: sms 서비스 활성화

--- src/main/kotlin/heispirate/cattower/domain/sms/service/SMSServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/sms/service/SMSServiceImpl.kt
@@ -8,29 +8,29 @@ import net.nurigo.sdk.message.response.SingleMessageSentResponse
 import net.nurigo.sdk.message.service.DefaultMessageService
 import org.springframework.stereotype.Service
 
-//@Service
-//class SMSServiceImpl(
-//    private val messageService: DefaultMessageService,
-//    private val smsRepository: SMSRepository
-//) : SMSService {
-//
-//    override fun sendOne(to: String, text: String): SingleMessageSentResponse? {
-//        val message = Message(
-//            from = "01065412527",
-//            to = to,
-//            text = text
-//        )
-//        val response = messageService.sendOne(SingleMessageSendingRequest(message))
-//        println(response)
-//
-//        if (response != null) {
-//            val sms = SMS(
-//                text = text,
-//                phoneNumber = to
-//            )
-//            smsRepository.save(sms)
-//        }
-//
-//        return response
-//    }
-//}
+@Service
+class SMSServiceImpl(
+    private val messageService: DefaultMessageService,
+    private val smsRepository: SMSRepository
+) : SMSService {
+
+    override fun sendOne(to: String, text: String): SingleMessageSentResponse? {
+        val message = Message(
+            from = "01065412527",
+            to = to,
+            text = text
+        )
+        val response = messageService.sendOne(SingleMessageSendingRequest(message))
+        println(response)
+
+        if (response != null) {
+            val sms = SMS(
+                text = text,
+                phoneNumber = to
+            )
+            smsRepository.save(sms)
+        }
+
+        return response
+    }
+}
--- src/main/kotlin/heispirate/cattower/infra/sms/SmsConfig.kt
+++ src/main/kotlin/heispirate/cattower/infra/sms/SmsConfig.kt
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-//@Configuration
-//class SmsConfig {
-//
-//    @Value("\${coolsms.api.key}")
-//    private lateinit var apiKey: String
-//
-//    @Value("\${coolsms.api.secret}")
-//    private lateinit var apiSecret: String
-//
-//    @Value("\${coolsms.api.url}")
-//    private lateinit var apiUrl: String
-//
-//    @Bean
-//    fun messageService(): DefaultMessageService {
-//        return initialize(apiKey, apiSecret, apiUrl)
-//    }
-//}
+@Configuration
+class SmsConfig {
+
+    @Value("\${coolsms.api.key}")
+    private lateinit var apiKey: String
+
+    @Value("\${coolsms.api.secret}")
+    private lateinit var apiSecret: String
+
+    @Value("\${coolsms.api.url}")
+    private lateinit var apiUrl: String
+
+    @Bean
+    fun messageService(): DefaultMessageService {
+        return initialize(apiKey, apiSecret, apiUrl)
+    }
+}
