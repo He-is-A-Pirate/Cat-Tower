9c2e973c509eeff38867332fbc1b57712e908211
kimboseong <njunju77@nate.com>
2024-06-19 12:35:39 +0900

    삭제: 커스텀쿼리메서드 미적용 문제로 삭제

--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomAuthCodeRepository.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomAuthCodeRepository.kt
@@ -3,5 +3,4 @@ package heispirate.cattower.domain.authCode.repository
 import java.time.LocalDateTime
 
 interface CustomAuthCodeRepository {
-    fun deleteByExpirationTime(zeroHour: LocalDateTime)
 }
--- src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomCustomAuthCodeRepositoryImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/repository/CustomCustomAuthCodeRepositoryImpl.kt
@@ -2,19 +2,11 @@ package heispirate.cattower.domain.authCode.repository
 
 import heispirate.cattower.domain.authCode.model.QAuthCode
 import heispirate.cattower.infra.QueryDslSupport
+import jakarta.transaction.Transactional
 import java.time.LocalDateTime
 import org.springframework.stereotype.Repository
 
 @Repository
 class CustomCustomAuthCodeRepositoryImpl : CustomAuthCodeRepository , QueryDslSupport() {
-    private val authCode = QAuthCode.authCode
-    override fun deleteByExpirationTime(zeroHour: LocalDateTime) {
-
-        queryFactory.delete(authCode)
-            .where(authCode.expirationTime.lt(zeroHour))
-            .execute()
-
-    }
-
 
 }
