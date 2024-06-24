6b135ff5910cd56b072664a51524e5d63d43d5d1
kimboseong <njunju77@nate.com>
2024-06-24 12:14:26 +0900

    수정: 인증코드 생성 서비스로 이관 및 이메일 유틸리티 이메일전송기능만 전담

--- src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeServiceImpl.kt
+++ src/main/kotlin/heispirate/cattower/domain/authCode/service/AuthCodeServiceImpl.kt
@@ -1,10 +1,13 @@
 package heispirate.cattower.domain.authCode.service
 
 import heispirate.cattower.domain.authCode.dto.AuthResponseDTO
+import heispirate.cattower.domain.authCode.model.AuthCode
 import heispirate.cattower.domain.authCode.repository.AuthCodeRepository
 import heispirate.cattower.infra.email.EmailUtility
 import jakarta.transaction.Transactional
 import java.time.LocalDateTime
+import java.util.UUID
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,7 +17,12 @@ class AuthCodeServiceImpl(
 ) : AuthCodeService {
     @Transactional
     override fun sendAuthEmail(email: String): AuthResponseDTO {
-        emailUtility.requestAuthCode(email)
+        val randomCode = generateCode(email)
+        emailUtility.sendEmail(
+            email = email,
+            subject = "캣타워 인증 코드 입니다",
+            text = "인증코드 : $randomCode"
+        )
         val message = "이메일 전송완료"
         return AuthResponseDTO(
             message = message
@@ -40,5 +48,57 @@ class AuthCodeServiceImpl(
     @Transactional
     override fun deleteAuthCode(zeroHour: LocalDateTime) {
        authCodeRepository.deleteByExpirationTime(zeroHour)
+    }
+
+    private fun generateCode(email: String):String{
+        val randomCode = UUID.randomUUID().toString().replace("-","").take(6)
+        val expirationTime = LocalDateTime.now().plusMinutes(5)
+        val authCode = authCodeRepository.findByEmailAndAvailableIsTrue(email)
+
+        when(authCode?.requestNumber){
+            null -> {
+                authCodeRepository.save(
+                    AuthCode(
+                        code = randomCode,
+                        expirationTime = expirationTime,
+                        email = email,
+                        requestNumber = 1,
+                        available = true
+                    )
+                )
+            }
+            1 ->{
+                authCode.available = false
+                authCodeRepository.save(authCode)
+                authCodeRepository.save(
+                    AuthCode(
+                        code = randomCode,
+                        expirationTime = expirationTime,
+                        email = email,
+                        requestNumber = 2,
+                        available = true
+                    )
+                )
+            }
+            2 -> {
+                authCode.available = false
+                authCodeRepository.save(authCode)
+                authCodeRepository.save(
+                    AuthCode(
+                        code = randomCode,
+                        expirationTime = expirationTime,
+                        email = email,
+                        requestNumber = 3,
+                        available = true
+                    )
+                )
+            }
+            3 ->{
+                throw Exception ("이메일 발송 횟수 초과입니다 익일 00시 이후에 시도해주세요")
+
+            }
+
+        }
+        return randomCode
     }
 }
--- src/main/kotlin/heispirate/cattower/infra/email/EmailUtility.kt
+++ src/main/kotlin/heispirate/cattower/infra/email/EmailUtility.kt
@@ -11,73 +11,14 @@ import org.springframework.stereotype.Component
 @Component
 class EmailUtility(
     val javaMailSender: JavaMailSender,
-    val authRepository: AuthCodeRepository
 ) {
 
-     fun requestAuthCode(email: String) {
-         sendEmail(email)
-     }
-
-
-    private fun generateCode(email: String):String{
-        val randomCode = UUID.randomUUID().toString().replace("-","").take(6)
-        val expirationTime = LocalDateTime.now().plusMinutes(5)
-        val authCode = authRepository.findByEmailAndAvailableIsTrue(email)
-
-        when(authCode?.requestNumber){
-            null -> {
-                authRepository.save(
-                    AuthCode(
-                        code = randomCode,
-                        expirationTime = expirationTime,
-                        email = email,
-                        requestNumber = 1,
-                        available = true
-                    )
-                )
-            }
-            1 ->{
-                authCode.available = false
-                authRepository.save(authCode)
-                authRepository.save(
-                    AuthCode(
-                        code = randomCode,
-                        expirationTime = expirationTime,
-                        email = email,
-                        requestNumber = 2,
-                        available = true
-                    )
-                )
-            }
-            2 -> {
-                authCode.available = false
-                authRepository.save(authCode)
-                authRepository.save(
-                    AuthCode(
-                        code = randomCode,
-                        expirationTime = expirationTime,
-                        email = email,
-                        requestNumber = 3,
-                        available = true
-                    )
-                )
-            }
-            3 ->{
-                throw Exception ("이메일 발송 횟수 초과입니다 익일 00시 이후에 시도해주세요")
-
-            }
-
-        }
-        return randomCode
-    }
-
-    private fun sendEmail(email:String){
-        val randomCode = generateCode(email)
+    fun sendEmail(email:String, subject: String,text:String){
 
         val message = SimpleMailMessage()
         message.setTo(email)
-        message.setSubject("캣타워 인증메일입니다")
-        message.setText("인증코드: $randomCode")
+        message.setSubject(" $subject ")
+        message.setText(" $text ")
         message.setFrom("rlaqhtjd8934@gmaicl.com")
         javaMailSender.send(message)
 
