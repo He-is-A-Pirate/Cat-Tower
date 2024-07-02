6928b4065ab68cdb401a6df92f2c264f9d30b38b
Karox1234 <zzzcomet@gmail.com>
2024-07-02 20:06:35 +0900

    수정 : admin을 객체로 사용하여 role 정보를 추가

--- src/main/kotlin/heispirate/cattower/domain/admin/dto/AdminResponseDTO.kt
+++ src/main/kotlin/heispirate/cattower/domain/admin/dto/AdminResponseDTO.kt
@@ -1,14 +1,17 @@
 package heispirate.cattower.domain.admin.dto
 
-import heispirate.cattower.domain.mainUser.model.MainUser
+import heispirate.cattower.domain.admin.model.Admin
+import heispirate.cattower.domain.admin.model.Role
 
 data class AdminResponseDTO(
-    val email: String
+    val email: String,
+    val role : Role
 ) {
     companion object {
-        fun fromMainUser(user: MainUser): AdminResponseDTO {
+        fun fromAdmin(admin: Admin): AdminResponseDTO {
             return AdminResponseDTO(
-                email = user.email
+                email = admin.mainUser.email,
+                role = admin.role
             )
         }
     }
