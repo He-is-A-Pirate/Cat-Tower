089e74a4ae49005a477648d97c124ae6911043dd
Karox1234 <zzzcomet@gmail.com>
2024-08-03 19:18:34 +0900

    추가 : 비동기 어노테이션 추가

--- src/main/kotlin/heispirate/cattower/CatTowerApplication.kt
+++ src/main/kotlin/heispirate/cattower/CatTowerApplication.kt
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @EnableJpaAuditing
+@EnableAsync
 class CatTowerApplication
 
 fun main(args: Array<String>) {
