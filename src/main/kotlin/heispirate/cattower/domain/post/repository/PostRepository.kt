deec32183c89aa990e70826848a75d81c1b97f44
Karox1234 <zzzcomet@gmail.com>
2024-08-03 19:20:22 +0900

    추가 : PostRepository 메서드 추가

--- src/main/kotlin/heispirate/cattower/domain/post/repository/PostRepository.kt
+++ src/main/kotlin/heispirate/cattower/domain/post/repository/PostRepository.kt
@@ -1,7 +1,11 @@
 package heispirate.cattower.domain.post.repository
 
 import heispirate.cattower.domain.post.model.Post
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PostRepository : JpaRepository<Post,Long> , CustomPostRepository{
+
+    fun findByPetProfileId(petProfileId: Long, pageable: Pageable): Page<Post>?
 }
